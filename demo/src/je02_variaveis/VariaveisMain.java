3da7a02c0f6ad5a09e3f2e20adeb7bca485bd108
eduardowalderrama <walderramaeduardo@gmail.com>
2024-10-19 14:20:20 -0300

    conceito de variaveis e metodos

---
+++ demo/src/je02_variaveis/VariaveisMain.java
@@ -0,0 +1,17 @@
+package je02_variaveis;
+
+public class VariaveisMain {
+    public static void main(String[] args) {
+        String nome = "Asher";
+        String BR = "Brasil";
+        double PI = 3.14;
+        int ESTADOS_BRASILEIRO = 27;
+        int ANO_2000 = 2000;
+
+        System.out.println(nome);
+        System.out.println(BR);
+        System.out.println(PI);
+        System.out.println(ESTADOS_BRASILEIRO);
+        System.out.println(ANO_2000);
+    }
+}
