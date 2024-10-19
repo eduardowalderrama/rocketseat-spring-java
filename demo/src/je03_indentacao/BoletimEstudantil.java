eaedaa7b16ea2f4ba5208e039bc5cd9b720f4c3f
eduardowalderrama <walderramaeduardo@gmail.com>
2024-10-19 14:29:04 -0300

    indentacao de codigos

---
+++ demo/src/je03_indentacao/BoletimEstudantil.java
@@ -0,0 +1,17 @@
+package je03_indentacao;
+
+import java.util.Scanner;
+
+public class BoletimEstudantil {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int mediaFinal = scanner.nextInt();
+
+        if (mediaFinal < 6)
+            System.out.println("REPROVADO");
+        else if (mediaFinal == 6)
+            System.out.println("PROVA MINERVA");
+        else
+            System.out.println("APROVADO");
+    }
+}
