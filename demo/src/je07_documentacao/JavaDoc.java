0cf78ddd83b293b8ad8c9a7150a6b362bf530ac5
eduardowalderrama <walderramaeduardo@gmail.com>
2024-10-19 15:48:12 -0300

    javadoc

---
+++ demo/src/je07_documentacao/JavaDoc.java
@@ -0,0 +1,10 @@
+package je07_documentacao;
+
+public class JavaDoc {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+
+        Integer resultado = calculadora.dividir(10, 5);
+        System.out.println(resultado);
+    }
+}
