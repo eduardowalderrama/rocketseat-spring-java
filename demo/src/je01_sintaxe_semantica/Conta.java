8f59f87552d9a24f708d357eb8631a6c34560567
eduardowalderrama <walderramaeduardo@gmail.com>
2024-11-10 15:13:20 -0300

    sintaxe e semantica

---
+++ demo/src/je01_sintaxe_semantica/Conta.java
@@ -0,0 +1,19 @@
+package je01_sintaxe_semantica;
+
+public class Conta {
+    //variavel da classe conta
+    double saldo=10.0;
+
+    public void sacar(Double valor) {
+    //variavel do metodo
+        double novoSaldo = saldo - valor;
+    }
+
+    public void imprimirSaldo() {
+        //disponível em toda classe
+        System.out.println(saldo);
+        //somente o metodo sacar conhece esta variavel
+        //System.out.println(novoSaldo);
+    }
+
+}
