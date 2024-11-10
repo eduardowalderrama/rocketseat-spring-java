95ea229d7cc0957325e9240bccb3b87cbeb18cde
eduardowalderrama <walderramaeduardo@gmail.com>
2024-11-10 15:47:00 -0300

    palavras reservadas e tipos primitivos

---
+++ demo/src/je04_palavras_reservadas/TiposPrimitivos.java
@@ -0,0 +1,17 @@
+package je04_palavras_reservadas;
+
+public class TiposPrimitivos {
+    public static void main(String[] args) {
+        int numero = 123;
+        double decimal = 123.0;
+        byte b = 127;
+        char c = 'a';
+        String nome = "a";
+
+        System.out.println(numero);
+        System.out.println(decimal);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(nome);
+    }
+}
