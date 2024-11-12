1d57893c7c1f3b3a4534f1182e0a89def657bb05
eduardowalderrama <walderramaeduardo@gmail.com>
2024-11-11 21:14:56 -0300

    javadoc

---
+++ demo/src/je07_documentacao/JavaDoc.java
@@ -0,0 +1,8 @@
+package je07_documentacao;
+
+public class JavaDoc {
+    public static void main(String[] args) {
+        Double resultado = Calculadora.dividir(10.0, 5.0);
+        System.out.println(resultado);
+    }
+}
