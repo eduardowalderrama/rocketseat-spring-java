f85af72b4ddf2174c85c220435cb4375661ef2c6
eduardowalderrama <walderramaeduardo@gmail.com>
2024-11-13 21:11:58 -0300

    break-continue - part 02

--- demo/src/je09_controle_fluxo/repeticao/ExemploBreakContinue.java
+++ demo/src/je09_controle_fluxo/repeticao/ExemploBreakContinue.java
@@ -4,10 +4,11 @@ public class ExemploBreakContinue {
     public static void main(String[] args) {
 
         for (int i = 1; i <= 5; i++) {
-            if (i == 3)
-                break;
-
             System.out.println(i);
+            if (i == 3)
+                continue;
+
+            System.out.println("execucao apos a regra");
         }
 
     }
