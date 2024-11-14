d0d4accf4a600a5e0d1f40b0b4e547b049dc3b60
eduardowalderrama <walderramaeduardo@gmail.com>
2024-11-13 21:08:05 -0300

    break-continue - part 01

---
+++ demo/src/je09_controle_fluxo/repeticao/ExemploBreakContinue.java
@@ -0,0 +1,14 @@
+package je09_controle_fluxo.repeticao;
+
+public class ExemploBreakContinue {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 5; i++) {
+            if (i == 3)
+                break;
+
+            System.out.println(i);
+        }
+
+    }
+}
