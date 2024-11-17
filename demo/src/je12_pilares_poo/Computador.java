797342d4a409472335b79d958aa4d75fc2fc486f
eduardowalderrama <walderramaeduardo@gmail.com>
2024-11-17 13:50:49 -0300

    encapsulamento

---
+++ demo/src/je12_pilares_poo/Computador.java
@@ -0,0 +1,9 @@
+package je12_pilares_poo;
+
+public class Computador {
+    public static void main(String[] args) {
+        MSNMessenger msnMessenger = new MSNMessenger();
+        msnMessenger.enviarMensagem();
+        msnMessenger.receberMensagem();
+    }
+}
---
+++ demo/src/je12_pilares_poo/MSNMessenger.java
@@ -0,0 +1,26 @@
+package je12_pilares_poo;
+
+public class MSNMessenger {
+    public void enviarMensagem() {
+        if (validarConectadoInternet())
+            System.out.println("Enviando mensagem");
+        else
+            System.out.println("Computador offline");
+    }
+
+    public void receberMensagem() {
+        if (validarConectadoInternet()) {
+            System.out.println("Recebendo mensagem");
+            salvarHistoricoMensagem();
+        }
+    }
+
+    private boolean validarConectadoInternet() {
+        System.out.println("Validando se esta conectado a internet");
+        return false;
+    }
+
+    private void salvarHistoricoMensagem() {
+        System.out.println("Salvando o historico da mensagem");
+    }
+}
