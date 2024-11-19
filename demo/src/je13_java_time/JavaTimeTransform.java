8ac7c4a097af5dc73ebe79010a0717c5b8f9c44e
eduardowalderrama <walderramaeduardo@gmail.com>
2024-11-18 21:29:32 -0300

    transformacao

---
+++ demo/src/je13_java_time/JavaTimeTransform.java
@@ -0,0 +1,17 @@
+package je13_java_time;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+public class JavaTimeTransform {
+    public static void main(String[] args) {
+        LocalDate localDate = LocalDate.now();
+        LocalTime localTime = LocalTime.of(19, 17, 21);
+        esperaDataHora(localDate.atTime(localTime));
+    }
+
+    static void esperaDataHora(LocalDateTime data) {
+        System.out.println(data);
+    }
+}
