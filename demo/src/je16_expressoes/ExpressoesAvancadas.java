4dd4029e3c134088a7f7c70e01bc15dd4a3b4daa
eduardowalderrama <walderramaeduardo@gmail.com>
2024-11-20 16:09:46 -0300

    expressoes avancadas

---
+++ demo/src/je16_expressoes/ExpressoesAvancadas.java
@@ -0,0 +1,9 @@
+package je16_expressoes;
+
+public class ExpressoesAvancadas {
+    public static void main(String[] args) {
+        String nome = "Anne";
+        String sobrenome = "Southport";
+        System.out.printf("%2$s %1$s%n", nome, sobrenome);
+    }
+}
